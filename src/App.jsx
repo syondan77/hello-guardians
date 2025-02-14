00409cd65fb4ec6eb22e67121341d0cbc73c3f5d
Dhanush Penumaka <dhanush.penumaka@toucanus.com>
2025-02-14 16:29:55 +0530

    Modified Images path

--- src/App.jsx
+++ src/App.jsx
@@ -4,25 +4,25 @@ function App() {
     return (
         <div className="contacts">
             <Contact
-                img="./src/images/5872824cf3a71010b5e8ef0f.png"
+                img="./5872824cf3a71010b5e8ef0f.png"
                 name="Mr. Captain America"
                 phone="(212) 555-1234"
                 email="CaptainLegen@super.avenger"
             />
             <Contact
-                img="./src/images/hulk.png"
+                img="./hulk.png"
                 name="Mr. Hulk"
                 phone="(312) 555-2345"
                 email="hulky@super.avenger"
             />
             <Contact
-                img="./src/images/thor.png"
+                img="./thor.png"
                 name="Mr. Thor"
                 phone="(412) 555-4567"
                 email="Mrhammer@super.avenger"
             />
             <Contact 
-                img="./src/images/panther.png"
+                img="./panther.png"
                 name="Mr. Black Panther"
                 phone="(1133) 121-9090"
                 email="panther@super.avenger"
